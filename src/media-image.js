1a50487f05d295a1c6daefbbde7bd5adbdb68570
Ian Melius <icm5131@psu.edu>
2024-04-12 22:49:04 -0400

    04/12 image hover

--- src/media-image.js
+++ src/media-image.js
@@ -10,7 +10,9 @@ export class mediaImage extends DDD {
 
     constructor() {
         super();
-        this.imgSrc = "https://i.pinimg.com/564x/b6/c0/2a/b6c02a4ad5b3538c4efb6aa194b432a8.jpg";
+        this.imgSrc = "";
+        this.caption = "Short image description";
+        this.description = "Full description of image";
     }
 
     static get styles() {
@@ -21,12 +23,18 @@ export class mediaImage extends DDD {
                     display: inline-flex;
                     flex-wrap: wrap;
                     --img-bord-color-primary: var(--ddd-theme-default-wonderPurple);
+                    --box-shadow: -1px 1px var(--ddd-theme-default-potentialMidnight),
+                    -2px 2px var(--ddd-theme-default-potentialMidnight),
+                    -3px 3px var(--ddd-theme-default-potentialMidnight),
+                    -4px 4px var(--ddd-theme-default-potentialMidnight),
+                    -5px 5px var(--ddd-theme-default-potentialMidnight),
+                    -6px 6px var(--ddd-theme-default-potentialMidnight);
                 }
 
                 .img-color-bord {
                     background-color: var(--img-bord-color-primary);
                     text-align: center;
-                    width: 30vw;
+                    width: 36vw;
                     padding: var(--ddd-spacing-4);
                     /* border */
                     border: var(--ddd-border-lg);
@@ -38,6 +46,15 @@ export class mediaImage extends DDD {
                     border: var(--ddd-border-lg);
                     border-color: var(--ddd-theme-default-potentialMidnight);
                     border-radius: var(--ddd-radius-md);
+                    width: 95%;
+                }
+
+                .img-color-bord:hover,
+                .img-color-bord:focus {
+                    box-shadow: var(--box-shadow);
+                    -webkit-transform: translate(var(--ddd-spacing-4));
+                    transform: translate(var(--ddd-spacing-4));
+                    transition: .5s ease;
                 }
             `
         ];
@@ -46,7 +63,7 @@ export class mediaImage extends DDD {
     render() {
         return html`
         <div class="img-color-bord">
-            <img class="image" src="${this.imgSrc}" alt="HTML and CSS meme">
+            <img class="image" src="${this.imgSrc}" alt="${this.caption}">
         </div>
         `;
     }
@@ -54,7 +71,9 @@ export class mediaImage extends DDD {
     static get properties() {
         return {
             ...super.properties,
-            imgSrc: { type: String, atribute: "img-src" }
+            imgSrc: { type: String, attribute: "img-src", reflect: true },
+            caption: { type: String },
+            description: { type: String }
         };
     }
 }
